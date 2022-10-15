92e7c22cff0054f16a76a9c44e1b092037aba89d
Danladi Etu <danladietugithub@gmail.com>
2022-10-14 17:49:36 -0700

    Warning correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 /* more headers goes there */
 /* main - Entry */
+/* Description: ' Positive anything is better than negative nothing'*/
 /* Return: Always 0*/
 /* betty style doc for function main goes there */
 int main(void)
