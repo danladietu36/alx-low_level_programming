03e326ac93f91f829b167205ee780c4caf62f986
Danladi Etu <danladietugithub@gmail.com>
2022-10-16 03:35:52 -0700

    5-print_number

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Alphabets in lower case, except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	}
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
