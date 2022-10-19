58065e7bb80fc37097a3c528604222a3518dcda6
Danladi Etu <danladietugithub@gmail.com>
2022-10-19 15:59:40 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integer
+ * @a: the first number
+ * @b: the second number
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
