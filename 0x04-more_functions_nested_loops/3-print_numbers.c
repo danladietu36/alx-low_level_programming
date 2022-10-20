e9a5f63a8147f3ae9e4c4017b3e740b628c04b58
Danladi Etu <danladietugithub@gmail.com>
2022-10-20 15:24:19 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	while (a < 10)
+		_putchar(a + '0');
+		a++;
+
+	_putchar('\n');
+}
