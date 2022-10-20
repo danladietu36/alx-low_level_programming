755907a58263e2b2f0e5af51f16d7fbafc6d67e0
Danladi Etu <danladietugithub@gmail.com>
2022-10-20 15:13:21 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: First of the parameters to multiply
+ * @b: Second parameter to multiply
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+
+}
