c574a9236d1e280e6a74ac53c18f4ea150448c48
Danladi Etu <danladietugithub@gmail.com>
2022-10-20 16:17:11 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int num, a;
+
+	a = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		num = 0;
+		for (num = 0; num = 14; num++)
+		{
+			_putchar(a + '0');
+		}
+	_putchar('\n');
+	}
+}
