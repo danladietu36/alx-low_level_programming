1b17108e6eeee64e2a73fbfc650f9df9e5762ef1
Danladi Etu <danladietugithub@gmail.com>
2022-10-28 13:53:19 -0700

    0-strcat.c3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,18 @@ char *_strcat(char *dest, char *src)
 	int n;
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	 n = 0;
+	while (dest[n] != '\0';)
+	{
+		n++;
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
 		dest[n] = src[i];
+		n++;
+		i++;
+	}
 	dest[n] = '\0';
-
 	return (dest);
 }
