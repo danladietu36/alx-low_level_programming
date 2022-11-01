ceed8d32ac343de4798332a0704b0316e03f5cf6
Danladi Etu <danladietugithub@gmail.com>
2022-11-01 14:39:50 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: array of string
+ * @b: character constant
+ * @n: size of memory area allocated to s
+ * Return: Always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*s = b;
+		i++;
+		_putchar(i);
+	}
+	return (s);
+}
