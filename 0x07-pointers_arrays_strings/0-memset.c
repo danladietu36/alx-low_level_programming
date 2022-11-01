0a6cf24cf9903a08cb8e5ba4a73b6ab3109c5c92
Danladi Etu <danladietugithub@gmail.com>
2022-11-01 15:04:19 -0700

    fills memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 
 /**
@@ -13,7 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 	int i;
 
 	i = 0;
-	while (i < 0)
+	while (i < n)
 	{
 		s[i] = b;
 		i++;
