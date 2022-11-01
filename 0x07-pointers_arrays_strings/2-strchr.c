c92be4a5d60f9fdb2bbe47664235817c50cd10ee
Danladi Etu <danladietugithub@gmail.com>
2022-11-01 16:00:24 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: array of characters
+ * @c: the character to locate
+ *
+ * Return: pointer to c
+ */
+char *_strchr(char *s, char c)
+{
+	int a, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+	return ('\0');
+}
