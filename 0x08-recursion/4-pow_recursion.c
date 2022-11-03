d3f850d0e3fa04d8ebeefafea0419118d61f9662
Danladi Etu <danladietugithub@gmail.com>
2022-11-03 16:58:45 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - function to return the value of number
+ * @x: value to return
+ * @y: exponential
+ *
+ * Return: Always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+	else
+		return (1)
+}
