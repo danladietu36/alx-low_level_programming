e9ecb3cd7cf00313b58c9380f9086cdf2895baae
Danladi Etu <danladietugithub@gmail.com>
2022-11-03 17:35:49 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+/**
+ * _sqrt_recursion - prints natural square root of a number
+ * @n: natural root
+ *
+ * Return: -1 on error and 0 on success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if ((root * root) == num)
+		return (root);
