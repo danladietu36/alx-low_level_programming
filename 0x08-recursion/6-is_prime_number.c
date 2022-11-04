a2601fbaf85c21eaefa05893d82982d1cb4449f2
Danladi Etu <danladietugithub@gmail.com>
2022-11-03 18:10:43 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime_number- get prime integer of a number
+ * @n: integer input
+ *
+ * Return: Always 0
+ */
+int is_prime_number(int n)
+{
+	int num;
+
+	if (num % n != 0)
+		return (1);
+	else
+		return (0);
+}
