d4aa36ad051de67deda1df73fad88b6c6fd65238
Danladi Etu <danladietugithub@gmail.com>
2022-11-07 08:01:06 -0800

    0-whatsmyname3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - contains arguments
@@ -10,12 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int counter;
+(void) argc;
+printf("%s\n",  argv[0]);
 
-	for (counter = 0; counter < argc; counter++)
-	{
-		printf(" %s\n",  argv[counter]);
-	}
-
-	return (0);
+return (0);
 }
