1d6cfd210f76e7c4661be69e1fb9075ec453a335
Danladi Etu <danladietugithub@gmail.com>
2022-11-07 07:16:09 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of argument passed into it
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
