08b2d6b52d2b391e4b92e1fd7cba129f76870be9
Danladi Etu <danladietugithub@gmail.com>
2022-11-09 19:57:26 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,18 +1,62 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * main - adds positive numbers
- * @argc: Argument counts
- * @argv: Argument vector
+ * check_number - check strings
+ * @str: array str
  *
  * Return: Always 0.
  */
-int main(int argc, char argv[])
+int check_number(char *str)
 {
-	int i, n, sum = 0;
+	unsigned int count;
 
-	for (i = 0; i < argc; i++)
+	count = 0;
+	while (count < strlen(str))
 	{
-		if (i 
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
 
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_number(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
