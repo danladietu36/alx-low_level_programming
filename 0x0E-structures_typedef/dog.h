f0d5327786eb1ab42ac2e14b9ee51a932dacf73d
Danladi Etu <danladietugithub@gmail.com>
2022-11-15 17:53:25 -0800

    no1again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,9 +3,9 @@
 
 /**
  * struct dog - the new struct name for the dog
- * @name: name of the dog
- * @age: age of dog
- * @owner: name of the owner
+ * @name: the name of the dog
+ * @age: the age of dog
+ * @owner: the name of the owner
  *
  */
 struct dog
@@ -21,6 +21,8 @@ struct dog
  */
 
 typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
