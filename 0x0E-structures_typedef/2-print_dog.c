c497af14e7c498e54afcf6e36242fe4045b454cd
Danladi Etu <danladietugithub@gmail.com>
2022-11-15 18:23:41 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,19 +9,12 @@
  */
 void print_dog(struct dog *d)
 {
-	if ( d == NULL)
+	if (d == NULL)
 		return;
 
-	if (d ->name == NULL)
-		printf("Name:(nil)\n");
-	else
-		printf("Name: %s\n", d -> name);
-	if (d -> age < 0)
-		printf("Age:(nil)\n");
-	else
-		printf("Age:(nil)\n", d ->age);
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->age == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner:%s\n", d->name, d->age, d->owner);
 }
