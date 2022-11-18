7858df217debae046ad0c4f61c2ecb700b52f556
Danladi Etu <danladietugithub@gmail.com>
2022-11-18 08:33:35 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: the array to search
+ * @size: size of the array
+ * @cmp: pointer to function to be used for comparism
+ *
+ * Return: returns element searched
+ * On error, return -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+					return (i);
+	}
+	return (-1);
+}
