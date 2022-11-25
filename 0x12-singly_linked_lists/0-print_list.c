7e7a83db4e811f9a2202e1e740f4d0b9e89383b4
Danladi Etu <danladietugithub@gmail.com>
2022-11-25 15:29:12 -0800

    task1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,11 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	if (!h->str)
-		printf("[0] (nil)");
-	else
-		printf("%u, %s", h->len, h->str);
-	s++;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d]  %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
 
 	return (s);
 }
