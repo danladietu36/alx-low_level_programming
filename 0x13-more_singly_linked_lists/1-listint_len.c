d44af62837e9fbcd66e7326dd82ef0c9c485f2d9
Danladi Etu <danladietugithub@gmail.com>
2022-11-29 14:12:29 -0800

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - function to return the number of elements
+ * @h: pointer to the head of the list
+ *
+ * Return: thr number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
