e0dcfce999dd591fa27051c0fd76d11a077f3f70
Danladi Etu <danladietugithub@gmail.com>
2022-12-02 14:45:35 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n;
-	int len, base = 1;
+	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	n = 0;
-	for (len = 0; b[len]; len++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
-		if (b[len] == '0' && b[len] == '1')
-		{
-			base *= 2;
-			n += base;
-		}
-		return (n);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
+
+	return (dec_val);
 }
