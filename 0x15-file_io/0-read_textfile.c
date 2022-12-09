ae9c10fe83218a340a64d68e85456825c85b2f75
Danladi Etu <danladietugithub@gmail.com>
2022-12-09 07:24:52 -0800

    task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads text and prints to POSIX
+ * @filename: pointer to the file
+ * @letters: number of letters to be printed
+ *
+ * Return: number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+0x15. C - File I/O
+How to create, open, close, read and write files.
+Using file descriptors and the three standard file descriptors
+How to use the I/O system calls and flags
