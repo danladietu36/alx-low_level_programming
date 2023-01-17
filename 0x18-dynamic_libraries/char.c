7dc7a7481ed74932066651ff468973f3942e67f1
Danladi Etu <danladietugithub@gmail.com>
2023-01-17 07:04:00 -0800

    libdynamic.so

---
+++ 0x18-dynamic_libraries/char.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+}
+char *_strcat(char *dest, char *src)
+{
+}
+char *_strncat(char *dest, char *src, int n)
+{
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
+char *_strchr(char *s, char c)
+{
+}
+void _puts(char *s)
+{
+}
+char *_strpbrk(char *s, char *accept)
+{
+}
+char *_strstr(char *haystack, char *needle)
+{
+}
---
+++ 0x18-dynamic_libraries/my_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+}
+int _islower(int c)
+{
+}
+int _isalpha(int c)
+{
+}
+int _abs(int n)
+{
+}
+int _isupper(int c)
+{
+}
+int _isdigit(int c)
+{
+}
+int _strlen(char *s)
+{
+}
+int _atoi(char *s)
+{
+}
+int _strcmp(char *s1, char *s2)
+{
+}
+unsigned int _strspn(char *s, char *accept)
+{
+}
