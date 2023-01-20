8245492429d9a0597a3a862726d35892ffd56a2b
Danladi Etu <danladietugithub@gmail.com>
2023-01-20 09:26:43 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/my_func.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * add - add two integers
+ * @a: first integers
+ * @b: second integers
+ * return: addition of both integers
+ *
+ */
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return (sum);
+}
+
+/*
+ * sub - subtraction function
+ * @a: first integers
+ * @b: second integers
+ * return: The result of both integers
+ *
+ */
+int sub(int a, int b)
+{
+	int res;
+	res = a - b;
+
+	return (res);
+}
