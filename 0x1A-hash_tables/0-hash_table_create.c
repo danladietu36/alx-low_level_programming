1812dc2132a1ee1dd1bed95f80f11c47347b6ea5
Danladi Etu <danladietugithub@gmail.com>
2023-02-07 14:12:31 -0800

    code_fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,6 +16,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/* allocate space for the hash table */
 	hash_table_created = malloc(sizeof(hash_table_t));
 
+	if (hash_table_created == NULL)
+		return (NULL);
+
 	hash_table_craeted->size = size;
 	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
 
