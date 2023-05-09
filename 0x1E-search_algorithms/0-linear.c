ef671c63c51ea6178b969c79ebcfac4892013d1b
Danladi Etu <danladietugithub@gmal.com>
2023-05-09 15:13:06 -0700

    added

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,16 +11,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
-	int flag = 0;
+	size_t i;
+	size_t flag = 0;
 
 	for (i =0; i < size; i++)
 	{
 		if (array[i] == value)
 		{
-			printf("Found element is %d\n" array[i]);
+			return (i);
 			flag = 1;
-			return (arr[i]);
 		}
 	}
 	if (array == NULL || flag == 0)
