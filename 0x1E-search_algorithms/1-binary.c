0020e2793e24c4d684ac0f989be6c09740cb4af3
Danladi Etu <danladietugithub@gmal.com>
2023-05-10 07:07:08 -0700

    added

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - function to to search sorted array of integers for a value
+ * @array: pointer to the first element of array to be search
+ * @size: number of elements in an array
+ * @value: value to search for
+ *
+ * Return: Always exited sucessful
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left, mid, right;
+	int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%i%s", array[i], i == right ? "\n" : ", ");
+		if (array[mid] < value)
+			left = mid + 1;
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
